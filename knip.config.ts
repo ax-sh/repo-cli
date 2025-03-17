cefb42191d246192fb39e4447a62a35ec4bea026
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-03-17 20:26:04 +0545

    chore: update knip config and add is-ci dependency

--- knip.config.ts
+++ knip.config.ts
@@ -1,8 +1,35 @@
 import type { KnipConfig } from 'knip'
 
 const config: KnipConfig = {
-  ignore: [],
-  entry: ['index.{js,ts}', 'src/index.{js,ts}'],
+  // "entry": ["src/index.ts", "scripts/{build,create}.js"],
+  eslint: {
+    config: ['eslint.config.mjs'],
+  },
+  ignore: ['.github/**/*', 'vitest.config.ts'],
+  ignoreDependencies: [
+    // Needed by eslint.
+    '@typescript-eslint/.*',
+    'eslint-.*',
+    'git-cliff',
+    'is-ci',
+
+    'tsx',
+  ],
+  vitest: {
+    config: [
+      'vitest.config.{js,mjs,ts,cjs,mts,cts}',
+      'vitest.{workspace,projects}.{js,mjs,ts,cjs,mts,cts,json}',
+    ],
+    entry: ['**/*.{bench,test,test-d,spec}.?(c|m)[jt]s?(x)'],
+  },
+  ignoreBinaries: ['rimraf', 'nr'],
+  // ignoreBinaries: ['tsx','rimraf', 'nlx', 'nr', 'act', 'storybook'],
+  entry: [
+    // 'index.{js,ts}',
+    // 'src/index.{js,ts}',
+    'src/**/*.{js,ts}',
+    // '**/*.{js,ts}'
+  ],
   project: ['**/*.{js,ts}'],
 }
 
