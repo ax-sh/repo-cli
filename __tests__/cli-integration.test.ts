eac205a98253598f40b78cbb9ec264eda822a47b
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-03-17 21:10:07 +0545

    test: update version checks in integration tests

--- __tests__/cli-integration.test.ts
+++ __tests__/cli-integration.test.ts
@@ -5,15 +5,16 @@ const src = filesystem.path(__dirname, '..')
 async function cli(cmd: string) {
   return system.run(`node ${filesystem.path(src, 'bin', 'repo')} ${cmd}`)
 }
+const version = '0.1.0'
 
 test('outputs version', async () => {
   const output = await cli('--version')
-  expect(output).toContain('0.0.1')
+  expect(output).toContain(version)
 })
 
 test('outputs help', async () => {
   const output = await cli('--help')
-  expect(output).toContain('0.0.1')
+  expect(output).toContain(version)
 })
 
 test.todo('generates file', async () => {
