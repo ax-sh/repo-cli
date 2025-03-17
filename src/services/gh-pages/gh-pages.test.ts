c69ef5983ee68ddacca192a082c6bf33ae503b0a
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-03-17 22:02:16 +0545

    test(gh-pages): add initial test for gh-pages module

--- src/services/gh-pages/gh-pages.test.ts
+++ src/services/gh-pages/gh-pages.test.ts
@@ -1,18 +1,5 @@
-import { print, system } from 'gluegun'
-import { KnownError } from '../../errors'
-
-export async function checkIfPushedToRemote() {
-  try {
-    const checkIfPushedToRemote = await system.run('git ls-remote', {
-      trim: true,
-    })
-    print.highlight({ checkIfPushedToRemote })
-  }
-  catch (e) {
-    throw new KnownError([
-      'Repo doesnt have associated github repo online',
-      // eslint-disable-next-line ts/no-unsafe-member-access
-      e.stderr,
-    ])
-  }
-}
+describe('ghpages', () => {
+  it('should load modules', () => {
+    expect(1).toBe(1)
+  });
+});
