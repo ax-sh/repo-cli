c725862f06dd2436f261b20878b386c0b7af37e0
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-03-17 22:05:39 +0545

    chore: update ts-morph-kit version and modify test description

--- src/services/gh-pages/gh-pages.test.ts
+++ src/services/gh-pages/gh-pages.test.ts
@@ -1,5 +1,7 @@
-describe('ghpages', () => {
-  it('should load modules', () => {
+describe.todo('gh-pages', () => {
+  it('should load modules', async () => {
+    // const i = await import('@ax-sh/ts-morph-kit')
+    // console.log(i);
     expect(1).toBe(1)
   });
 });
