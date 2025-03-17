618debaa7ac600e8b262a24b0c577b690f503c66
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-03-17 20:13:48 +0545

    feat: add knip

---
+++ knip.config.ts
@@ -0,0 +1,9 @@
+import type { KnipConfig } from 'knip'
+
+const config: KnipConfig = {
+  ignore: [],
+  "entry": ["index.{js,ts}", "src/index.{js,ts}"],
+  "project": ["**/*.{js,ts}"]
+}
+
+export default config
