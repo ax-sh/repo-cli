d3fc3100ce0da536c10861f2fd6e04b77e53926d
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-03-18 16:28:28 +0545

    chore: update dependencies and modify tests

--- src/services/gh-pages/gh-pages.test.ts
+++ src/services/gh-pages/gh-pages.test.ts
@@ -1,7 +1,14 @@
+import ts from '@ax-sh/ts-morph-kit'
+
 describe('gh-pages', () => {
   it('should load modules', async () => {
-    const ts = await import('@ax-sh/ts-morph-kit')
-    console.debug(ts)
-    expect(ts).toBeDefined()
+    // const ts = await import('@ax-sh/ts-morph-kit')
+    const file = '/Users/axm/Desktop/monorepo-nx/repo-cli/src/services/gh-pages/gh-pages.test.ts'
+    const sourceFile = ts.openAsSourceFile(file)
+
+    console.debug(sourceFile.getText())
+    expect(a).toBeDefined()
+    // const sf = ts.openAsSourceFile('gh-pages.test.ts')
+    // console.log(sf.getText());
   })
 })
