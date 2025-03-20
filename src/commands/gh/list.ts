79fc137e21c357d63b7f57dad75effedf9605916
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-03-21 04:29:19 +0545

    feat: add gh list command

---
+++ src/commands/gh/list.ts
@@ -0,0 +1,16 @@
+import type { GluegunCommand } from 'gluegun'
+import type { ExtendedToolbox } from '../../types'
+
+const command: GluegunCommand<ExtendedToolbox> = {
+  name: 'gh',
+  run: async (toolbox) => {
+    const { print, parameters } = toolbox
+    const lang = parameters.first
+
+    const root = await import('../../services/gh.service')
+    const out = await root.listAllPublicRepos(lang)
+    print.highlight(out)
+  },
+}
+
+export default command
