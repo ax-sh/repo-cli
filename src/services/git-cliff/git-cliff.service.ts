09520724fc2f73c950c25d0aefb309352e49306c
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-03-21 17:20:48 +0545

    feat(git-cliff): update git cliff scripts with options

--- src/services/git-cliff/git-cliff.service.ts
+++ src/services/git-cliff/git-cliff.service.ts
@@ -3,8 +3,8 @@ import { addScriptToPackageJson } from '../../lib/helpers/cmd/cli'
 
 export async function addGitCliffScriptsToPackageJson() {
   let script: string
-  script = await addScriptToPackageJson('cll', 'git cliff -l')
+  script = await addScriptToPackageJson('cll', 'git cliff -l -s all')
   print.highlight(script)
-  script = await addScriptToPackageJson('cl', 'git cliff -u')
+  script = await addScriptToPackageJson('cl', 'git cliff -u -s all')
   print.highlight(script)
 }
