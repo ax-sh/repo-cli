e6d99b584ba5a064dd04813725c4db6ee89a3adf
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-03-21 16:23:45 +0545

    test(issues): update tests for issue creation functionality

--- src/services/issues/issues.test.ts
+++ src/services/issues/issues.test.ts
@@ -1,7 +1,6 @@
-import g from 'gluegun';
+import g from 'gluegun'
 
 vi.mock('gluegun', { spy: true })
-
 describe('[issues] service test', () => {
   it('should ', async () => {
     const mod = await import('./issues.service')
@@ -11,15 +10,19 @@ describe('[issues] service test', () => {
     console.warn(out)
   });
   it('should create issues', async () => {
+    const cmd = 'gh issue create -a @me -t test_title -b test_body '
     const fn = vi.mocked(g.system.run)
-    fn.mockResolvedValue('https://github.com/user/repo/issues/mocked')
+    fn.mockImplementation(async (args) => {
+      expect(args).toEqual(cmd);
+      return 'https://github.com/user/repo/issues/mocked'
+    })
     const mod = await import('./issues.service')
 
-    const out = await mod.createIssue('title', 'body')
+    const out = await mod.createIssue('test_title', 'test_body')
     expect(out).toEqual('https://github.com/user/repo/issues/mocked');
     expect(fn).toBeCalled()
     expect(fn).toBeCalledTimes(1)
-    expect(fn).toBeCalledWith('gh issue create -a @me -t title -b body ', {
+    expect(fn).toBeCalledWith(cmd, {
       trim: true,
     })
 
