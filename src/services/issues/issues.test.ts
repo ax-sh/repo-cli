b1165e6d3ec422bd949d7377ed8323e0783b525d
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-03-21 16:33:42 +0545

    test(issues): add tests for createIssue functionality

--- src/services/issues/issues.test.ts
+++ src/services/issues/issues.test.ts
@@ -1,7 +1,33 @@
 import g from 'gluegun'
+import { exeCmdWithOutput } from '../../lib';
+import { createIssue } from './issues.service';
 
 vi.mock('gluegun', { spy: true })
+vi.mock('../../lib/helpers/cmd/cli', async () => {
+  return {
+    exeCmdWithOutput: vi.fn(),
+    getJsonFromCmd: vi.fn(),
+  };
+});
 describe('[issues] service test', () => {
+  beforeEach(() => {
+    // Clear all mocks before each test
+    vi.clearAllMocks();
+  });
+
+  afterEach(() => {
+    // Restore mocks after each test
+    vi.restoreAllMocks();
+  });
+  it('should work', async () => {
+    const fn = vi.mocked(exeCmdWithOutput)
+    fn.mockImplementation(async (args) => {
+      return Promise.resolve(args);
+    })
+    const out = await createIssue('test_title', 'test_body')
+    expect(fn).toBeCalled()
+    expect(out).toEqual('gh issue create -a @me -t test_title -b test_body ')
+  });
   it('should ', async () => {
     const mod = await import('./issues.service')
     expect(mod).toBeDefined()
