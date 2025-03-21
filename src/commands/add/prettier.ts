ae385e8c0554071516ba235d0b71199e0b81c87a
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-03-21 06:18:44 +0545

    feat: add prettier sub command

---
+++ src/commands/add/prettier.ts
@@ -0,0 +1,37 @@
+import type { GluegunCommand } from 'gluegun'
+import type { ExtendedToolbox } from '../../types';
+
+const command: GluegunCommand<ExtendedToolbox> = {
+  name: 'prettier',
+  alias: ['pretty'],
+  run: async (toolbox) => {
+    const { print, system, template, filesystem } = toolbox
+
+    const spinner = print.spin('Adding prettier deps')
+
+    await toolbox.lib.exeCmdWithOutput('ni -D prettier pretty-quick @trivago/prettier-plugin-sort-imports')
+    await toolbox.lib.addScriptToPackageJson('prettier', 'prettier . --check')
+    await toolbox.lib.addScriptToPackageJson('prettier:fix', 'prettier . --write')
+    await toolbox.lib.addScriptToPackageJson('pretty', 'pretty-quick')
+
+    spinner.info('Added prettier deps')
+
+    const fileNames = ['.prettierrc', '.prettierignore']
+    for (const fileName of fileNames) {
+      await template.generate({
+        // directory: "CONFIGS/tailwind",
+        template: `/CONFIGS/prettier/${fileName}`,
+        target: filesystem.path('.', fileName),
+      })
+    }
+
+    spinner.info('Added prettier configs')
+    spinner.stopAndPersist()
+    await system.run('nr prettier . --write')
+
+    // const root = await import('../services/prettier.service')
+    // await root.run();
+  },
+}
+
+export default command;
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,6 @@
 import appRootPath from 'app-root-path'
 
-export { addScriptToPackageJson } from './helpers/cmd/cli';
+export { addScriptToPackageJson, exeCmdWithOutput } from './helpers/cmd/cli';
 
 export { appRootPath }
 
