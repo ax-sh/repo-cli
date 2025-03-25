115b086780994494812d20e20ce0dce1e1b7100c
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-03-25 17:10:48 +0545

    test(go): add tests for mocked filesystem and cmd output

--- src/services/go/go.test.ts
+++ src/services/go/go.test.ts
@@ -1,9 +1,31 @@
+import { filesystem } from 'gluegun';
+import { expect } from 'vitest';
+import { exeCmdWithOutput } from '../../lib';
+
+vi.mock('../../lib/helpers/cmd/cli')
+// @ts-expect-error not a error
+vi.mock('gluegun', ({ filesystem: { isFile: vi.fn() } }))
+
 describe('[go] service test', () => {
-  it.fails('should addCodeQualityTools', async () => {
+  it('should check if filesystem has been mocked properly', () => {
+    // eslint-disable-next-line ts/unbound-method
+    const fn = vi.mocked(filesystem.isFile)
+
+    expect(fn).toHaveProperty('mock');
+  });
+  it('should addCodeQualityTools', async () => {
     const mod = await import('./go.service')
     expect(mod).toBeDefined()
+    // eslint-disable-next-line ts/unbound-method
+    vi.mocked(filesystem.isFile).mockReturnValue(true)
 
+    const fn = vi.mocked(exeCmdWithOutput)
+    expect(fn).toHaveProperty('mock');
+    fn.mockImplementation(async (args: string) => args)
     const out = await mod.addCodeQualityTools()
-    expect(out).toEqual('not go repo')
+    expect(fn).toBeCalled();
+    expect(fn).toHaveBeenCalledTimes(4)
+    expect(fn).toHaveResolved()
+    expect(out).toMatchSnapshot()
   })
 })
