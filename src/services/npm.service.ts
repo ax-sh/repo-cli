d5d6060951cfd126d0153865f0887f12afb2d74c
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-04-15 15:31:24 +0545

    feat(npm.service): enhance whoamiGithub with error handling

--- src/services/npm.service.ts
+++ src/services/npm.service.ts
@@ -3,7 +3,18 @@ import { KnownError } from '../errors'
 
 const auth = '--//npm.pkg.github.com/:_authToken="$(gh auth token)"'
 export async function whoamiGithub() {
-  return system.run(`npm whoami ${auth}`, { trim: true })
+  const { $, ExecaError } = await import('execa');
+  try {
+    return await $`npm whoami ${auth}`
+  }
+  catch (error) {
+    if (error instanceof ExecaError) {
+      print.error(error.message)
+      print.info(`${error.shortMessage} ${error.durationMs}`)
+      print.highlight(error.escapedCommand);
+      return error.failed
+    }
+  }
 }
 
 // "gh:pub": "NPM_CONFIG_REGISTRY=\"https://npm.pkg.github.com/\" npm publish --//npm.pkg.github.com/:_authToken=\"$(gh auth token)\"",
@@ -13,7 +24,7 @@ export async function publishToGithubPrivateRegistry() {
   // eslint-disable-next-line node/prefer-global/process
   process.env.NPM_CONFIG_REGISTRY = 'https://npm.pkg.github.com/'
   const user = await whoamiGithub()
-  if (user !== 'ax-sh') {
+  if (user && user !== 'ax-sh') {
     throw new KnownError([user, 'Not the right github account auth'])
   }
   const pub = await system.run(`npm publish ${auth}`, { trim: true })
