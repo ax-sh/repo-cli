8dbe186323fa6bb767734cc93a3d1080c95e5921
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-04-15 23:13:00 +0545

    feat(info): add print message after info retrieval

--- src/commands/info.ts
+++ src/commands/info.ts
@@ -4,8 +4,10 @@ import type { ExtendedToolbox } from '../types';
 const command: GluegunCommand<ExtendedToolbox> = {
   name: 'info',
   run: async (toolbox) => {
+    const { print } = toolbox
     const root = await import('../services/info/info.service')
     await root.getInfo();
+    print.info('done')
   },
 }
 
