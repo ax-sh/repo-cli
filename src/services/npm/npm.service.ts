7a8317f48715032255c08e38a00a77551ddf53c4
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-04-15 18:49:10 +0545

    refactor(npm.service.ts): simplify GitHub NPM auth handling

--- src/services/npm/npm.service.ts
+++ src/services/npm/npm.service.ts
@@ -18,40 +18,38 @@ async function checkGithubAuthStatus() {
     return false
   }
 }
-
-const auth = '--//npm.pkg.github.com/:_authToken="$(gh auth token)"'
-export async function whoamiGithub() {
-  if (!await checkGithubAuthStatus()) {
-    throw new KnownError('gh Auth Token is invalid')
-  }
-  const { $ } = await import('execa')
-
+async function exeCmdWithOutputWithGithubNpmAuth(cmd: string) {
+  const auth = '--//npm.pkg.github.com/:_authToken="$(gh auth token)"'
   // eslint-disable-next-line node/prefer-global/process
   process.env.NPM_CONFIG_REGISTRY = 'https://npm.pkg.github.com/'
   try {
-    const { stdout: auth } = await $`gh auth token`
-    const who = await exeCmdWithOutput(`npm whoami --//npm.pkg.github.com/:_authToken="${auth}"`)
-    return who
+    return await exeCmdWithOutput(`${cmd} ${auth}`)
   }
   catch (e) {
     print.error(e.stderr);
     print.highlight(e.cmd)
-    return ''
-    // throw new KnownError(e)
+    throw new KnownError(e)
   }
+}
+
+export async function whoamiGithub() {
+  if (!await checkGithubAuthStatus()) {
+    throw new KnownError('gh Auth Token is invalid')
+  }
+
+  const who = await exeCmdWithOutputWithGithubNpmAuth('npm whoami')
+  return who
 }
 
 // "gh:pub": "NPM_CONFIG_REGISTRY=\"https://npm.pkg.github.com/\" npm publish --//npm.pkg.github.com/:_authToken=\"$(gh auth token)\"",
 // "pub": "nr prepublishOnly && npm publish --scope=@ax-sh --registry=https://npm.pkg.github.com",
 // ^ this does not work as expected as above
 export async function publishToGithubPrivateRegistry() {
-  // eslint-disable-next-line node/prefer-global/process
-  process.env.NPM_CONFIG_REGISTRY = 'https://npm.pkg.github.com/'
   const user = await whoamiGithub()
   if (user !== 'ax-sh') {
     throw new KnownError([user, 'Not the right github account auth'])
   }
-  const pub = await exeCmdWithOutput(`npm publish ${auth}`)
+  const pub = await exeCmdWithOutputWithGithubNpmAuth(`npm publish`)
   console.warn('Publishing to', user)
   print.highlight(pub)
 }
