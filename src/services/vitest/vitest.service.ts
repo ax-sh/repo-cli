19a23686ada0a0a3dee7fc7485efb75eff3fde7b
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-04-17 17:54:38 +0545

    feat(vitest): add vitest scripts to package.json

--- src/services/vitest/vitest.service.ts
+++ src/services/vitest/vitest.service.ts
@@ -1,19 +1,28 @@
 import { filesystem, print } from 'gluegun'
 
-import { exeCmdWithOutput } from '../../lib'
+import { addScriptToPackageJson, exeCmdWithOutput } from '../../lib'
 import { addVitestReactTypesToTsconfig } from '../tsconfig/tsconfig.service'
 
 export async function addVitestDeps() {
-  return exeCmdWithOutput('ni -D vitest msw@latest @faker-js/faker')
+  const out = await exeCmdWithOutput('ni -D vitest msw@latest @faker-js/faker')
+  await addScriptToPackageJson('test', 'dotenv -- vitest run')
+  // await toolbox.addScriptToPackageJson('test', 'vitest run')
+  await addScriptToPackageJson('test:watch', 'vitest')
+  await addScriptToPackageJson('test:snapupdate', 'vitest -u')
+  await addScriptToPackageJson('coverage', 'vitest run --coverage')
+  return out
 }
 
 export async function addVitestWithReactTesting() {
   await exeCmdWithOutput('ni -D vitest @testing-library/user-event @testing-library/react @testing-library/dom @types/react @types/react-dom msw@latest @faker-js/faker @testing-library/jest-dom')
+
+  // modify tsconfig for react and vite
   const tsconfigPath = 'tsconfig.app.json'
   const currentData = filesystem.read(tsconfigPath)!
   const updatedData = await addVitestReactTypesToTsconfig(currentData)
   filesystem.write(tsconfigPath, updatedData)
-  // todo
+
+  // todo type can be omitted as the above adds it on tsconfig
   // /// <reference types="vitest" />
   //
   // import { defineConfig } from "vitest/config";
