e89ec791125ada152f78a3654378c068508bbb7e
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-04-17 15:14:37 +0545

    feat(tailwind): integrate tailwindcss into project setup

--- src/services/tailwind/tailwind.service.ts
+++ src/services/tailwind/tailwind.service.ts
@@ -1,12 +1,18 @@
-import { addPluginsInDefaultViteConfig } from '@ax-sh/ts-morph-kit'
+import { addImportsToSourceFile, addPluginsInDefaultViteConfig } from '@ax-sh/ts-morph-kit'
 import { openViteConfigAsSourceFile } from '../gh-pages/gh-pages.service'
 
 export async function addTailwindToProject() {
   const sourceFile = openViteConfigAsSourceFile()
+
+  addImportsToSourceFile(sourceFile, [
+    {
+      imports: 'tailwindcss',
+      from: '@tailwindcss/vite',
+    },
+  ])
   addPluginsInDefaultViteConfig(sourceFile, ['tailwindcss()'])
-  // addImportsToTsFile(sourceFile, "")
   sourceFile.formatText()
   sourceFile.saveSync()
 
-  return 'done'
+  return 'Add @import "tailwindcss"; to your main css file'
 }
