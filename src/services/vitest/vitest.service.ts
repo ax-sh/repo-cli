c1fdde05d6446c7711a05c99a4097afaea8c8afc
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-04-17 16:52:00 +0545

    chore: add todo comments for vitest setup and tests

--- src/services/vitest/vitest.service.ts
+++ src/services/vitest/vitest.service.ts
@@ -8,6 +8,38 @@ export async function addVitestDeps() {
 export async function addVitestWithReactTesting() {
   await exeCmdWithOutput('ni -D vitest @testing-library/react @testing-library/dom @types/react @types/react-dom msw@latest @faker-js/faker @testing-library/jest-dom')
 
+  // todo
+  // /// <reference types="vitest" />
+  //
+  // import { defineConfig } from "vitest/config";
+  //
+  // export default defineConfig({
+  //   test: {
+  //     globals: true,
+  //     environment: "jsdom",
+  //     setupFiles: "./vitest.setup.ts",
+  //   },
+  // });
+  // root file ./vitest.setup.ts
+  // import "@testing-library/jest-dom/vitest";
+  // import { afterEach } from "vitest";
+  // import { cleanup } from "@testing-library/react";
+  //
+  // // runs a clean after each test case (e.g. clearing jsdom)
+  // afterEach(() => {
+  //   cleanup();
+  // });
+  // app.test.tsx
+  // import { render, screen } from "@testing-library/react";
+  // import App from "./App";
+  //
+  // describe("App", () => {
+  //   it("renders the App component", () => {
+  //     render(<App />);
+  //
+  //     screen.debug(); // prints out the jsx in the App component unto the command line
+  //   });
+  // });
   print.info('done')
   const out = 'out'
   return out
