9ea0da836ee9e49015afd3d0c56ebb3fce88815e
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-04-19 17:50:46 +0545

    test(vite-plugins): add unit tests for Vite plugins service

--- src/services/vite-plugins/vite-plugins.test.ts
+++ src/services/vite-plugins/vite-plugins.test.ts
@@ -1,8 +1,34 @@
+import {
+  createTestSourceFile,
+  getDefaultViteConfig,
+  objectLiteralExpressionToJson,
+  openAsSourceFile,
+} from '@ax-sh/ts-morph-kit'
+
+import { expect } from 'vitest'
 import { exeCmdWithOutput } from '../../lib';
 
 vi.mock('../../lib/helpers/cmd/cli')
+vi.mock('@ax-sh/ts-morph-kit', { spy: true })
+const code = `
+import { defineConfig } from 'vite';
+// https://vite.dev/config/
+export default defineConfig({
+  plugins:[foo()],
+  base:"test",
+  ___test___: true
+});
+`
 
 describe('[vite-plugins] service test', () => {
+  beforeEach(async () => {
+    const mocked = vi.mocked(openAsSourceFile)
+    expect(mocked).toHaveProperty('mock')
+
+    mocked.mockReturnValue(
+      await createTestSourceFile(code),
+    );
+  });
   it('should return results', async () => {
     const fn = vi.mocked(exeCmdWithOutput)
     fn.mockImplementation(async (args: string) => args)
@@ -10,6 +36,9 @@ describe('[vite-plugins] service test', () => {
     expect(mod).toBeDefined()
 
     const out = await mod.addDefaultVitePlugins()
-    console.warn(out)
+    const sf = await createTestSourceFile(out)
+    const viteDefaultConfigJson = objectLiteralExpressionToJson(getDefaultViteConfig(sf))
+
+    expect(viteDefaultConfigJson).toEqual({ plugins: ['foo()', 'qrcode()'], base: 'test', ___test___: true })
   });
 });
