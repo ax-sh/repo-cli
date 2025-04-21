114c463e7b6dcf8bd815ed2e71ebf76e585be1e8
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-04-21 17:02:11 +0545

    feat: add configuration functions for Vite pages plugin

--- src/services/vite-plugin-pages/vite-plugin-pages.service.ts
+++ src/services/vite-plugin-pages/vite-plugin-pages.service.ts
@@ -1,16 +1,39 @@
-import { addImportsToSourceFile, addPluginsInDefaultViteConfig } from '@ax-sh/ts-morph-kit'
+import { addImportsToSourceFile, addPluginsInDefaultViteConfig, openAsSourceFile } from '@ax-sh/ts-morph-kit'
 import { print } from 'gluegun'
 import { exeCmdWithOutput } from '../../lib'
 import { openViteConfigAsSourceFile } from '../gh-pages/gh-pages.service'
 
-export async function addVitePluginPages() {
-  await exeCmdWithOutput('ni -D vite-plugin-pages')
-  await exeCmdWithOutput('ni react-router react-router-dom')
+function configureViteConfigWithVitePagesPlugin() {
   const sourceFile = openViteConfigAsSourceFile()
-  addImportsToSourceFile(sourceFile, [{ imports: 'Pages', from: 'vite-plugin-pages' }])
+  addImportsToSourceFile(sourceFile, [
+    { imports: 'Pages', from: 'vite-plugin-pages' },
+  ])
   addPluginsInDefaultViteConfig(sourceFile, ['Pages()'])
   sourceFile.formatText()
   sourceFile.saveSync()
+}
+
+function configureViteReactMainFile() {
+  const mainFile = 'src/main.tsx'
+  const sourceFile = openAsSourceFile(mainFile)
+
+  addImportsToSourceFile(sourceFile, [
+    { imports: ['HashRouter'], from: 'react-router-dom' },
+  ])
+  sourceFile.formatText()
+  sourceFile.saveSync()
+  print.highlight(` 
+   <HashRouter basename="/">
+     <App />
+   </HashRouter>
+  `)
+}
+
+export async function addVitePluginPages() {
+  await exeCmdWithOutput('ni -D vite-plugin-pages')
+  await exeCmdWithOutput('ni react-router react-router-dom react-error-boundary')
+  configureViteConfigWithVitePagesPlugin()
+  configureViteReactMainFile()
 
   print.info('ref: https://www.npmjs.com/package/vite-plugin-pages')
   const out = 'done'
