7f734e5f39288c4ad9dbcdbf9b4a099adaa69f93
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-04-21 20:16:08 +0545

    feat(renovate): update command for GitHub platform usage

--- src/commands/renovate.ts
+++ src/commands/renovate.ts
@@ -5,7 +5,8 @@ const command: GluegunCommand<ExtendedToolbox> = {
   name: 'renovate',
   run: async (toolbox) => {
     const { print, lib } = toolbox
-    const cmd = 'bunx renovate --token $(gh auth token) --autodiscover'
+    const cmd = 'bunx renovate --platform=github --token $(gh auth token) $(gh repo view --json nameWithOwner -q .nameWithOwner)'
+    // const cmd = 'bunx renovate --token $(gh auth token) --autodiscover'
     print.highlight(cmd)
     const out = await lib.exeCmdWithOutput(cmd)
     const spinner = print.spin('Running renovate on all the allowed repos')
