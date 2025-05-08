1943d45c184f1cf7718f1ab689f4ce2f0f75e659
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-05-08 14:08:08 +0545

    refactor(eslint): update console rules and config structure

--- src/services/eslint/eslint.service.ts
+++ src/services/eslint/eslint.service.ts
@@ -5,13 +5,14 @@ const code = `
 // https://github.com/antfu/eslint-config
 import antfu from '@antfu/eslint-config'
 
+/** @type {import('eslint').Linter.Config} */
 const globalConfig = {
   rules: {
     'style/semi': ['off', 'never'],
     'toml/array-bracket-newline': 'off',
     'ts/no-misused-promises': ['off'],
     'ts/restrict-template-expressions': ['off'],
-    'no-console': ['warn', { allow: ['table', 'debug', 'warn', 'error'] }],
+    'no-console': ['warn', { allow: ['table', 'debug', 'warn', 'error', 'info', 'time', 'timeEnd', 'dir'] }],
   },
 };
 export default antfu(
@@ -19,9 +20,11 @@ export default antfu(
     type: 'app',
     test: true,
     typescript: { tsconfigPath: 'tsconfig.json' },
+    // typescript: { parserOptions: { project: 'tsconfig.json' } },
     // \`.eslintignore\` is no longer supported in Flat config, use \`ignores\` instead
     ignores: [
       '**/fixtures',
+      './public'
       // ...globs
     ],
   },
@@ -31,7 +34,8 @@ export default antfu(
 `
 export async function addEslint() {
   const out = await exeCmdWithOutput('ni -D eslint @antfu/eslint-config eslint-config-biome @biomejs/biome')
-  filesystem.write('./eslint.config.mjs', code)
+  const eslintConfigFile = './eslint.config.mjs'
+  filesystem.write(eslintConfigFile, code)
   await addScriptToPackageJson('lint', 'eslint')
   await addScriptToPackageJson('lint:fix', 'eslint --fix')
 
