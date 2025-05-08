1aabb245da633574c430f260821b8e6b1facab05
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-05-08 14:55:56 +0545

    chore: add add command

--- src/commands/add/add.ts
+++ src/commands/add/add.ts
@@ -4,19 +4,21 @@ import type { ExtendedToolbox } from '../../types'
 const command: GluegunCommand<ExtendedToolbox> = {
   name: 'add',
   run: async (toolbox) => {
-    const { print, parameters, system } = toolbox
+    const { print, parameters, prompt } = toolbox
     const commandName = parameters.first?.trim()
     if (commandName == null) {
       print.printHelp(toolbox)
       print.highlight('USE repo --compiled-build TO USE BUILT VERSION')
       return
     }
-    const spinner = print.spin()
-    const root = await import('../../services/analytics/analytics.service')
-    const out = await root.run()
-    spinner.succeed()
-    print.highlight(`TODO Out ADD ${out}`)
-    await system.run('echo ni -D husky')
+    const confirmed = await prompt.confirm(`[${commandName}] Command does not exist, create it?`)
+    if (confirmed) {
+      const spinner = print.spin()
+      // const root = await import('../../services/analytics/analytics.service')
+      // const out = await root.run()
+      spinner.succeed(`TODO Out ADD new command ${commandName}`)
+      spinner.stop()
+    }
   },
 }
 
