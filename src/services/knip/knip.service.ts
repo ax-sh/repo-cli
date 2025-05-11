765d02f8c3a7597db1fd3773756eeda161972e24
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-05-11 17:55:38 +0545

    refactor(knip.service.ts): simplify addKnip function output

--- src/services/knip/knip.service.ts
+++ src/services/knip/knip.service.ts
@@ -55,17 +55,7 @@ const knipConfigFile = './knip.config.ts'
 
 export async function addKnip() {
   filesystem.write(knipConfigFile, knipConfigFileContents)
-  // const hasError = Boolean(input)
-  // if (hasError) {
-  //   await wait(1000)
-  //   const error = 'todo handle error knip '
-  //   return error
-  // }
-  //
-  // console.debug('doing')
-  // print.info('done')
-  let out: string
-  out = 'Run something knip out'
-  out = 'todo handle success knip out'
+
+  const out: string = 'Added Knip Config'
   return out
 }
