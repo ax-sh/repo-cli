bc00d74d60d098f0dd9732999ff5354f723189a4
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-05-11 17:52:35 +0545

    chore(knip.config.ts): update configuration options and paths

--- knip.config.ts
+++ knip.config.ts
@@ -4,9 +4,22 @@ const config: KnipConfig = {
   $schema: 'https://unpkg.com/knip@5/schema.json',
   entry: ['index.{js,ts}', 'src/**/*.{ts,js,mjs,cjs}'],
   project: [
+    'src/**/*.ts',
     'tsconfig.base.json', // Essential for resolving path aliases
   ],
-
+  prettier: true,
+  storybook: false,
+  lefthook: false,
+  drizzle: false,
+  playwright: false,
+  'graphql-codegen': false,
+  nx: false,
+  prisma: false,
+  msw: false,
+  tsx: false,
+  tsup: false,
+  unocss: false,
+  tailwind: false,
   typescript: true,
   vite: true,
   vitest: true,
@@ -20,7 +33,7 @@ const config: KnipConfig = {
     // CLI is often a dev dependency used in scripts
   ],
   ignoreDependencies: [
-    // Add other dev tools or type packages here if Knip reports them as unused
+    // Add other dev tools or type packages here if Knip reports them as unused,
     // but they are necessary for your development workflow.
     'tsx',
     '@typescript-eslint/*',
